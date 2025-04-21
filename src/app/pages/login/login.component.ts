92457270271ad8b6f96cca54a2bda949ba7c42df
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-04-21 17:45:33 +0200

    setting up the secure auth portal

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Login submitted:', this.form.value);
+      // Later: Call AuthService and navigate to dashboard
+    }
+  }
+}
