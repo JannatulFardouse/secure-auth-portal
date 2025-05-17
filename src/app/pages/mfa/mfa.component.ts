f4738a346271ef3bcd50326a20514463f0f671ad
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-05-17 08:15:50 +0200

    added mfa for authenticator

--- src/app/pages/mfa/mfa.component.ts
+++ src/app/pages/mfa/mfa.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { authenticator } from 'otplib';
 
 @Component({
   selector: 'app-mfa',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './mfa.component.html',
-  styleUrl: './mfa.component.scss'
+  styleUrls: ['./mfa.component.scss']
 })
 export class MfaComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      token: ['', Validators.required]
+    });
+  }
+
+  verify() {
+    const username = localStorage.getItem('tempUser');
+    const userData = localStorage.getItem(username!);
+    if (!userData) return;
+
+    const user = JSON.parse(userData);
+    const isValid = authenticator.check(this.form.value.token, user.secret);
+
+    if (isValid) {
+      localStorage.setItem('authToken', username!);
+      alert('Authentication successful');
+      window.location.href = '/dashboard';
+    } else {
+      alert('Invalid token');
+    }
+  }
 }
