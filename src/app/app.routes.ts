067bfa6328bd186883ad1bddb02cf2e9ecb196f4
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-05-17 08:15:17 +0200

    update rout and active auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { MfaComponent } from './pages/mfa/mfa.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {path: 'mfa', component:MfaComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate:[authGuard] },
 ];
